1594300c166174239909aa300b60c4a9e375d774
Madison Evans <m.corbinevans@gmail.com>
2023-04-12 16:10:08 -0700

    working on sign up form

---
+++ client/src/Components/CreateAccountForm.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CreateAccountForm = () => {
+	return (
+		<div className="w-full h-full text-center text-2xl text-info">
+			CreateAccount
+		</div>
+	);
+};
+
+export default CreateAccountForm;
