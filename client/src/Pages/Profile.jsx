4918b7972fa2bee910dd347cb367c37ccdcdc995
Madison Evans <m.corbinevans@gmail.com>
2023-04-12 08:44:12 -0700

    adding patch functionality to profile

--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -14,63 +14,84 @@ const Profile = () => {
 	const [showBioEditor, setShowBioEditor] = useState(false);
 	const [showSongEditor, setShowSongEditor] = useState(false);
 	const [showUserTypeEditor, setShowUserTypeEditor] = useState(false);
-	const data = {
-		genres: ["hip-hop", "r&b"],
-		social_links: ["https://twitter.com/", "https://soundcloud.com/"],
-		song_sample: "https://soundcloud.com",
-	};
+
+	// TODO: bio editor
 	function toggleBioEditor() {
 		console.log("edit clicked");
 		setShowBioEditor((prev) => !prev);
 	}
-	function patchBio() {
-		console.log("patch clicked");
+	const patchBio = async () => {
+		const newBio = "this is my new bio";
+		const requestBody = {
+			bio: newBio,
+		};
+		const requestOptions = {
+			method: "PATCH",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(requestBody),
+			credentials: "include",
+		};
+		const response = await fetch(`/users/${user.id}`, requestOptions);
+		const jsonResponse = await response.json();
+		console.log(jsonResponse);
 		toggleBioEditor();
-	}
+	};
+
+	// TODO: Song editor --> need to find workaround for how db is set up
 	function toggleSongEditor() {
-		console.log("edit clicked");
 		setShowSongEditor((prev) => !prev);
 	}
-	function patchSong() {
-		console.log("patch clicked");
+	const patchSong = async () => {
+		//
+		const newSong = "new song url? ";
+		const requestBody = {
+			song_sample: newSong,
+		};
+		const requestOptions = {
+			method: "PATCH",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(requestBody),
+			credentials: "include",
+		};
+
+		console.log("patch clicked\n", "need to find workaround for db");
 		toggleSongEditor();
-	}
+	};
+
+	// TODO: UserType Editor
 	function toggleUserTypeEditor() {
 		console.log("edit clicked");
 		setShowUserTypeEditor((prev) => !prev);
 	}
-	function patchUserType() {
-		console.log("patch clicked");
+	const patchUserType = async () => {
+		const newUserTypeID = 3;
+		const requestBody = {
+			user_type_id: newUserTypeID,
+		};
+		const requestOptions = {
+			method: "PATCH",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(requestBody),
+			credentials: "include",
+		};
+		const response = await fetch(`/users/${user.id}`, requestOptions);
+		const jsonResponse = await response.json();
+		console.log(jsonResponse);
 		toggleUserTypeEditor();
-	}
+	};
+
 	return (
 		<>
 			{user && (
 				<>
 					<div className="flex flex-row justify-center w-full h-full text-info">
-						<div className="w-1/3 h-full p-4 flex flex-col justify-start">
-							<div className="overflow-hidden relative rounded-md text-primary h-[460px] flex flex-col justify-end">
-								<img
-									src={user.profile_picture_url}
-									alt="prof pic"
-									className="w-full h-full absolute object-cover"
-								/>
-								<div className="w-full h-full absolute bg-gradient-to-t from-[#00000055] to-fuchsia-[#00000000]" />
-							</div>
-							<div className="z-50 px-4">
-								<h1 className="text-4xl font-display mt-6 text-info">
-									{user.artist_name}
-								</h1>
-								<p className="my-2 font-thin italic">{user.user_type.name}</p>
-							</div>
-							<ul className="text-sm p-4 rounded-md h-1/4 flex flex-col justify-start mb-4 font-thin text-accent">
-								<li>{user.location}</li>
-								{data.genres.map((genre, key) => {
-									return <div key={key}>{genre}</div>;
-								})}
-								<li>Searching for a user.in_search_of</li>
-							</ul>
-						</div>
+						{ArtistInfo()}
 
 						<div
 							className="w-2/3 h-full pr-4 flex flex-col justify-start 
@@ -102,7 +123,7 @@ const Profile = () => {
 								{showSongEditor ? (
 									<ProfileInfoEditor patchFunction={patchSong} />
 								) : (
-									<p>song</p>
+									<p>{user.song_sample}</p>
 								)}
 								<div className="absolute right-0 -top-10 my-2">
 									<EditButton
@@ -120,61 +141,93 @@ const Profile = () => {
 								{showUserTypeEditor ? (
 									<ProfileInfoEditor patchFunction={patchUserType} />
 								) : (
-									<p>Producer</p>
+									<p>{user.user_type.name}</p>
 								)}
 								<div className="absolute right-0 -top-10 my-2">
 									<EditButton
 										callback={toggleUserTypeEditor}
-										buttonText="Edit Song Demo"
+										buttonText="Edit User Type"
 									/>
 								</div>
 							</div>
-							<div className="w-full h-full relative flex flex-col">
-								<h2 className="text-2xl font-display text-center my-4 text-accent">
-									Recent Matches
-								</h2>
-								<div className=" h-full w-full grid grid-cols-3 gap-2 justify-between items-center rounded-lg mb-12">
-									{/* {matches &&
-										matches.map((match, key) => (
-											<motion.div
-												initial={{ scale: 1 }}
-												whileHover={{
-													scale: 1.1,
-													transition: { type: "spring", duration: 0.4 },
-												}}
-												key={key}
-												className="
-            								group cursor-pointer mx-auto h-[100px] w-[120px] rounded overflow-y-auto relative">
-												<div
-													className="
-												absolute w-full h-full bg-[#000000aa] flex flex-row opacity-0 group-hover:opacity-100  transition-opacity duration-300">
-													<div
-														className="
-													w-full my-auto text-white text-center">
-														<h2 className="text-xl font-thin">
-															{match.artist_name}
-														</h2>
-														<p className="text-sm font-thin">
-															{match.user_type.name}
-														</p>
-													</div>
-												</div>
-												<img
-													className="
-												w-full h-full object-cover"
-													src={match.profile_picture_url}
-													alt={match.artist_name}
-												/>
-											</motion.div>
-										))} */}
-								</div>
-							</div>
+							{RecentMatches()}
 						</div>
 					</div>
 				</>
 			)}
 		</>
 	);
+
+	function RecentMatches() {
+		return (
+			<div className="w-full h-full relative flex flex-col">
+				<h2 className="text-2xl font-display text-center my-4 text-accent">
+					Recent Matches
+				</h2>
+				<div className=" h-full w-full grid grid-cols-3 gap-2 justify-between items-center rounded-lg mb-12">
+					{/* {matches &&
+            matches.map((match, key) => (
+                <motion.div
+                    initial={{ scale: 1 }}
+                    whileHover={{
+                        scale: 1.1,
+                        transition: { type: "spring", duration: 0.4 },
+                    }}
+                    key={key}
+                    className="
+                group cursor-pointer mx-auto h-[100px] w-[120px] rounded overflow-y-auto relative">
+                    <div
+                        className="
+                    absolute w-full h-full bg-[#000000aa] flex flex-row opacity-0 group-hover:opacity-100  transition-opacity duration-300">
+                        <div
+                            className="
+                        w-full my-auto text-white text-center">
+                            <h2 className="text-xl font-thin">
+                                {match.artist_name}
+                            </h2>
+                            <p className="text-sm font-thin">
+                                {match.user_type.name}
+                            </p>
+                        </div>
+                    </div>
+                    <img
+                        className="
+                    w-full h-full object-cover"
+                        src={match.profile_picture_url}
+                        alt={match.artist_name}
+                    />
+                </motion.div>
+            ))} */}
+				</div>
+			</div>
+		);
+	}
+
+	function ArtistInfo() {
+		return (
+			<div className="w-1/3 h-full p-4 flex flex-col justify-start">
+				<div className="overflow-hidden relative rounded-md text-primary h-[460px] flex flex-col justify-end">
+					<img
+						src={user.profile_picture_url}
+						alt="prof pic"
+						className="w-full h-full absolute object-cover"
+					/>
+					<div className="w-full h-full absolute bg-gradient-to-t from-[#00000055] to-fuchsia-[#00000000]" />
+				</div>
+				<div className="z-50 px-4">
+					<h1 className="text-4xl font-display mt-6 text-info">
+						{user.artist_name}
+					</h1>
+					<p className="my-2 font-thin italic">{user.user_type.name}</p>
+				</div>
+				<ul className="text-sm p-4 rounded-md h-1/4 flex flex-col justify-start mb-4 font-thin text-accent">
+					<li>{user.location}</li>
+					<li>hip hop</li>
+					<li>Searching for a user.in_search_of</li>
+				</ul>
+			</div>
+		);
+	}
 };
 
 export default Profile;
