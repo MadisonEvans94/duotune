c453f6971e0c0e5de5f68b3ec17df40552dacede
Madison Evans <m.corbinevans@gmail.com>
2023-04-12 16:37:25 -0700

    fixed sign in bug

--- client/src/Components/ChatRoomContent.jsx
+++ client/src/Components/ChatRoomContent.jsx
@@ -63,7 +63,7 @@ const ChatRoomContent = ({
 									isUser ? "justify-end" : "justify-start"
 								} mb-4`}>
 								<p
-									className={`px-4 py-2 rounded-lg ${
+									className={`px-4 py-2 rounded-lg shadow-[6px_6px_0px_0px_rgba(0,0,0,1)] ${
 										isUser ? "bg-accent text-info" : "bg-gray-200 text-dark"
 									}`}>
 									{message.content}
